cbc4129c6d4bb4db90a3391d65a33e49be1a691c
Martijn <mvancooten@davinci.nl>
2014-04-12 12:03:50 +0200

    first program

---
+++ src/Welcome.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Martijn
+ */
+public class Welcome {
+            public static void main(String[] args) {
+                         // Display message Welcome to Java! on the console
+                         System.out.println("Welcome to Java!");
+            }
+}
+ 
